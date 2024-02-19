fad42bff5b47e02b6e54354c0595ca471c3480fa
Sam Walton <samwalton9@gmail.com>
2024-02-19 16:38:01 +0000

    Goals now flexible about destination scene

--- Features/Goal.gd
+++ Features/Goal.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@export var destination_scene : PackedScene
+
 
 func _on_area_2d_area_entered(_area):
-	Events.change_scene.emit("res://Menus/WinMenu.tscn")
+	Events.change_scene.emit(destination_scene.resource_path)
 	Events.game_end.emit()
